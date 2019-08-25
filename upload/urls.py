b90cecfb847ebee179459b61f2bfc0c9cf5ea54e
Ayush Priya <ayushpriya10@gmail.com>
2019-08-26 00:28:44 +0530

    Fixed import in upload/urls.py

--- upload/urls.py
+++ upload/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListRidesView, FileUploadView, FlushDatabaseView, BulkFileUploadView, TaskStatus, AsyncFileHandler
+from .views import ListRidesView, FlushDatabaseView, TaskStatus, AsyncFileHandler
 
 
 urlpatterns = [
