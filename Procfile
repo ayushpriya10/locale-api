98516421dddbc632c1025ff2c7dec1b6781ed4d9
Ayush Priya <ayushpriya10@gmail.com>
2019-08-26 00:26:50 +0530

    Amended file upload taking too long.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn api.wsgi -b 0.0.0.0:$PORT --log-level debug
+web: gunicorn api.wsgi -b 0.0.0.0:$PORT --log-level debug -t 3600
 worker: celery -A api worker -l info -c 4 --without-heartbeat
--- upload/urls.py
+++ upload/urls.py
@@ -4,9 +4,7 @@ from .views import ListRidesView, FileUploadView, FlushDatabaseView, BulkFileUpl
 
 urlpatterns = [
     path('list/', ListRidesView.as_view(), name="list-all-rides"),
-    path('upload/', FileUploadView.as_view(), name='upload-file'),
-    path('bulk/', BulkFileUploadView.as_view(), name='upload-bulk-create'),
     path('flushdb/', FlushDatabaseView.as_view(), name='flush-db'),
-    path('async/', TaskStatus.as_view(), name='async-task-status'),
+    path('task_status/', TaskStatus.as_view(), name='async-task-status'),
     path('asyncfile/', AsyncFileHandler.as_view(), name='async-file-handler')
 ]
